82c7e0a606fea42872c030ba2f9bbadce113c3c6
subliminalvisions <subliminalvisions@gmail.com>
2018-09-02 17:45:38 -0600

    updated directory name, added 2017 theme for comparison

---
+++ wp-content/themes/bootstrap-basetheme-1a/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+function bootstrapstarter_enqueue_styles() {
+    wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    $dependencies = array('bootstrap');
+	wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies ); 
+}
+
+function bootstrapstarter_enqueue_scripts() {
+    $dependencies = array('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_scripts' );
+
+function bootstrapstarter_wp_setup() {
+    add_theme_support( 'title-tag' );
+}
+
+add_action( 'after_setup_theme', 'bootstrapstarter_wp_setup' );
+
+
+add_action( 'init', 'add_slug_body_class' );
+	// ADD post name in body class
+//Page Slug Body Class
+function add_slug_body_class( $classes ) {
+global $post;
+if ( isset( $post ) ) {
+$classes[] = $post->post_type . '-' . $post->post_name;
+}
+return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
+
+// Change RSS Cache to 10 min
+add_filter( 'wp_feed_cache_transient_lifetime', 
+   create_function('$a', 'return 900;') );
+
+?>
